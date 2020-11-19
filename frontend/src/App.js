c73b8e2c2c65ca371e81ca81fe342261db49c231
Matt Higgins <mhiggins.louis@gmail.com>
2020-11-19 11:58:51 -1000

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../src/components/Header";
+import Footer from "../src//components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+
+import "./bootstrap.min.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to the shop</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/" component={HomeScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
